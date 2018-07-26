ee9bc9379fe5a34a76576221ce8a6d4d966067f2
lemonsqueeze <lemonsqueeze@gmx.com>
2018-07-26 16:00:40 +0200

    luci: update page (fake wget)

--- src/usr/lib/lua/luci/controller/admin/quotas.lua
+++ src/usr/lib/lua/luci/controller/admin/quotas.lua
@@ -4,5 +4,19 @@ function index()
     entry({"admin", "quotas"}, alias("admin", "quotas", "template"), "Quotas", 60).index = true
 
     entry({"admin", "quotas", "status"}, template("admin_quotas/status"), "Status", 1)
+    entry({"admin", "quotas", "update"}, template("admin_quotas/update"), "Update", 2)
+
+    -- for update page
+    entry({"admin", "quotas", "install"}, call("action_install"), nil).leaf = true
 end
 
+function action_install()
+        local file = luci.http.formvalue("file")
+	luci.http.prepare_content("text/plain")
+	local r = luci.sys.call("/usr/share/download_quotas/luci/install " .. file)
+	if r == 0 then
+		luci.http.write("Success !")
+        else
+		luci.http.write("Failed ...")
+	end
+end
