e642cd790fb94b71c168ea3e91decbd40b7490ca
lemonsqueeze <lemonsqueeze@gmx.com>
2018-07-28 11:53:21 +0200

    luci: status tab

--- src/usr/lib/lua/luci/controller/admin/quotas.lua
+++ src/usr/lib/lua/luci/controller/admin/quotas.lua
@@ -2,5 +2,7 @@ module("luci.controller.admin.quotas", package.seeall)
 
 function index()
     entry({"admin", "quotas"}, alias("admin", "quotas", "template"), "Quotas", 60).index = true
+
+    entry({"admin", "quotas", "status"}, template("admin_quotas/status"), "Status", 1)
 end
 
